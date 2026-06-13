a4f0e143b4045d9b61e893aedaf9cbf655f5347a
Jaiko <cnfl1ct.nw@gmail.com>
2026-06-12 20:18:28 -0400

    modified:   src/kernel/core/kernel.c

--- src/kernel/core/kernel.c
+++ src/kernel/core/kernel.c
@@ -46,8 +46,8 @@ void kmain(u32 magic, multiboot_info_t *mbi)
 
     KLOG_LOG("Welcome to MiniOS!");
 
-    log_set_hide_info(1);
-    log_set_hide_all(1);
+    log_set_hide_info(0);
+    log_set_hide_all(0);
 
     kernel_init();
 
